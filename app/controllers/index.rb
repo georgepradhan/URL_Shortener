a62041b069ef063396098515392eb9e0d2433a97
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-07 10:33:51 -0700

    controller routes and view for url shortener homepage

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,18 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/urls' do
+  #instantiate a URL object and in doing so generates short url (via before_save method)
+  long_url = params[:long_url].gsub(/http:\/\//, '')
+  url = Url.new
+  url.long_url = long_url
+  url.save
+  @short = url.short_url
+  erb :index
+end
+
+get '/:short_url' do
+  row = Url.find_by_short_url(params[:short_url])
+  redirect "http://#{row.long_url}"
+end
