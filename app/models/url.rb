d55253e5146fd18600de00d1a680db24cc75c371
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-07 11:35:00 -0700

    add validations to url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,9 @@
 class Url < ActiveRecord::Base
   require 'digest/sha1'
 
+  validates :long_url, presence: true
+  validates :long_url, length: { minimum: 1 }
+
   before_save :generate_short_url
 
   private
